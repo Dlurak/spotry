5c5d3984096320220826595d0ac48ace44f1e055
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-04-24 04:18:56 +0200

    Fix problems with the refresh tokens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,38 @@
 <script lang="ts">
 	import Navbar from '$lib/components/layout/Navbar.svelte';
+	import { newAccessToken } from '$lib/spotify/accessToken';
+	import { svocal } from '$lib/svocal';
+	import { nowInSeconds } from '$lib/utils/dates/convert';
 	import '../app.css';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData
+
+	const msToSec = 1_000_000;
+
+	const accessTokenExpires = svocal('accessTokenExpires');
+	const accessToken = svocal('accessToken');
+	const refreshToken = svocal('refreshToken');
+
+	$: secondsUntilAccessTokenExpires = nowInSeconds() - $accessTokenExpires;
+
+	const refresh = async () => {
+		if (!$refreshToken) return;
+
+		await newAccessToken({
+			refreshToken: $refreshToken,
+			base64Encoded: data.base64
+		}).then((data) => {
+			accessToken.set(data.access_token);
+			accessTokenExpires.set(nowInSeconds() + data.expires_in);
+		});
+		window.alert($accessToken)
+
+		setTimeout(refresh, ($accessTokenExpires - 20) * msToSec);
+	};
+
+	// 20 second before the token expires we try to get a new one
+	setTimeout(refresh, Math.max(0, secondsUntilAccessTokenExpires - 20) * msToSec);
 </script>
 
 <Navbar />

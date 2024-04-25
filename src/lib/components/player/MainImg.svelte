930d905a1db19e8c93714ded3e555413cc24171c
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-04-25 21:18:32 +0200

    Fix aspect ratios

--- src/lib/components/player/MainImg.svelte
+++ src/lib/components/player/MainImg.svelte
@@ -7,42 +7,45 @@
 		width: number;
 	};
 
+	const aspectRatio = img.width / img.height;
+
+	let imgEleHeight: number | null = null;
+
 	export let isPaused: boolean;
 </script>
 
 <div class="flex h-full w-full items-center justify-center">
-	<div
-		class="relative z-50 h-full overflow-hidden rounded-xl object-contain transition-all"
-		use:tilt={{
-			reverse: true,
-			strength: 0.5
-		}}
-	>
-		<img
-			src={img.url}
+	<div bind:clientHeight={imgEleHeight} class="flex h-full items-center justify-center">
+		<div
 			style:--w={img.width}
 			style:--h={img.height}
-			alt="The album cover"
-			class="aspect-[--w/--h] h-full object-contain"
-		/>
-		<div
-			class="absolute inset-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 saturate-50"
-			class:hidden={!isPaused}
+			style:--width={imgEleHeight ? `min(${imgEleHeight * aspectRatio}px, 100%)` : 'unset'}
+			class="relative aspect-[--w/--h] w-[--width] overflow-hidden rounded-xl shadow-2xl"
+			use:tilt={{
+				reverse: true,
+				strength: 0.5
+			}}
 		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="h-48 w-48 text-white"
+			<img src={img.url} alt="The album cover" />
+			<div
+				class="absolute inset-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50 saturate-50"
+				class:hidden={!isPaused}
 			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M14.25 9v6m-4.5 0V9M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-				/>
-			</svg>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="h-48 w-48 text-white"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M14.25 9v6m-4.5 0V9M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+			</div>
 		</div>
 	</div>
 </div>
